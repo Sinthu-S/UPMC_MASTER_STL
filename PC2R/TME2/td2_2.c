3f37aeb197446da7ea17e641582d30cf19cbd3dc
SIVANATHAN SINTHUJAN <3100129@ppti-14-408-06.ufr-info-p6.jussieu.fr>
2016-01-25 17:54:16 +0100

    Tme2 pc2r

---
+++ PC2R/TME2/td2_2.c
@@ -0,0 +1,56 @@
+/******************************* td2_2.canevas *******************************/
+
+#include "fthread.h"
+#include "stdio.h"
+#include "unistd.h"
+#include "traceinstantsf.h"
+
+ft_event_t  evt;
+
+void awaiter (void *args)
+{
+  int i, res;
+ 
+  for (i = 0; i< 5; i++) {
+    fprintf(stdout, "debut awater\n");
+     res = ft_thread_await(evt);
+     if(res == OK)
+       printf("event received !\n");
+     if( res == ETIMEOUT)
+       printf("timeout \n");
+    ft_thread_cooperate_n(3);
+  }  
+  fprintf(stdout, "stop generator!\n");
+  ft_scheduler_stop((ft_thread_t)args);
+ 
+}
+
+void generator (void *args)
+{
+  int i;
+
+  for (i=0;; ++i) {
+    ft_thread_cooperate_n(7);
+    ft_thread_generate(evt);
+  }
+}
+
+
+
+int main (void)
+{
+  ft_thread_t ft_trace, ft_awaiter, ft_generator;
+  ft_scheduler_t sched = ft_scheduler_create ();
+
+  evt = ft_event_create(sched);
+  ft_trace = ft_thread_create(sched, tanceinstants, NULL, (void *)50);
+
+  ft_generator = ft_thread_create(sched, generator, NULL, NULL);
+  ft_awaiter = ft_thread_create(sched, awaiter, NULL, ft_generator);
+   
+  ft_scheduler_start(sched);
+  
+
+  ft_exit ();
+  return 0;
+}
